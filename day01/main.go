ca4f3c7d21a3fb02a69feeebd5de3bad79d50944
Kevin Wang <kevin@xorkevin.com>
2025-12-01 01:10:47 -0800

    Finish day01

--- day01/main.go
+++ day01/main.go
@@ -25,16 +25,57 @@ func main() {
 		}
 	}()
 
+	start := 50
+	zeroCount := 0
+	zeroCount2 := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		num, err := strconv.Atoi(scanner.Text())
+		line := scanner.Text()
+		var isLeft bool
+		switch line[0] {
+		case 'L':
+			isLeft = true
+		case 'R':
+			isLeft = false
+		default:
+			log.Fatalln("Invalid direction")
+		}
+		num, err := strconv.Atoi(line[1:])
 		if err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Println(num)
+
+		zeroCount2 += num / 100
+		num = num % 100
+		if num > 0 {
+			if isLeft {
+				if start == 0 {
+					start = 100 - num
+				} else {
+					start -= num
+				}
+			} else {
+				start += num
+			}
+			if start == 0 || start == 100 {
+				start = 0
+				zeroCount += 1
+				zeroCount2 += 1
+			} else if start < 0 {
+				start += 100
+				zeroCount2 += 1
+			} else if start > 100 {
+				start -= 100
+				zeroCount2 += 1
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
 	}
+
+	fmt.Println("Part 1:", zeroCount)
+	fmt.Println("Part 2:", zeroCount2)
 }
